3da9cdeebd29f00c7ef8adfddadbb03f4bf9404e
adhikareeprayush <adhikareeprayush@gmail.com>
2025-01-11 10:48:45 +0545

    tasks

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -7,13 +7,24 @@ const Task = () => {
     id: string;
     title: string;
     category: string;
-    createdDate: string;
+    createdOn: string;
     modifiedDate: string;
     dueDate: string;
     timeSpent: string;
     notes: string;
   }
-  
+
+  const [coverImg, setCoverImg] = useState<string>(
+    "https://images.unsplash.com/photo-1731432245362-26f9c0f1ba2f?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  );
+
+  const changeCover = () => {
+    const newCover = prompt("Enter new cover image URL");
+    if (newCover) {
+      setCoverImg(newCover);
+    }
+  };
+
   const [task, setTask] = useState<TaskType | null>(null);
 
   useEffect(() => {
@@ -34,13 +45,20 @@ const Task = () => {
   return (
     <div className="min-h-screen flex flex-col items-start justify-start ml-[300px] flex-1">
       <div className="flex flex-col border-b-[1px] border-slate-200 w-full pb-3">
-        <div className="w-full h-[320px] bg-red-500">
-          <img src="https://images.unsplash.com/photo-1731432245362-26f9c0f1ba2f?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="w-full h-full object-cover" />
+        <div className="w-full h-[320px] bg-red-500 relative">
+          <img src={coverImg} alt="" className="w-full h-full object-cover" />
+          <button
+            className="absolute top-4 right-4 bg-slate-100 text-slate-400 px-2 py-1 rounded-md"
+            onClick={() => changeCover()}
+          >
+            Change Cover
+          </button>
         </div>
         <div className="flex flex-col w-full p-4">
-        <h3 className="text-2xl ">
-          {task.title}
-        </h3>
+          <h3 className="text-2xl ">{task.title}</h3>
+          <span className="border-[1px] bg-slate-100 border-slate-200 rounded-md px-2 cursor-pointer w-fit">
+            {task.createdOn}
+          </span>
         </div>
       </div>
     </div>
